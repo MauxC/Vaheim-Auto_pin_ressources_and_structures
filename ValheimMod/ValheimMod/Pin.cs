d83785fa52163c7206fd43b412f0607fa3926a1a
MauxC <72012637+MauxC@users.noreply.github.com>
2023-02-16 14:50:50 +0100

    ADD: mod files

---
+++ ValheimMod/ValheimMod/Pin.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace ValheimMod
+{
+    public struct Pin
+    {
+        public enum PinType
+        {
+            Mineral,
+            Structure,
+            Spawner,
+            Undefined = default
+        }
+
+        public Pin(string label, string compName, bool save, bool name, bool enabled, PinType type)
+        {
+            Label = label;
+            ComponentName = compName;
+            Save = save;
+            PrintName = name;
+            Enabled = enabled;
+            Type = type;
+        }
+
+        public string Label { get; }
+        public string ComponentName { get; }
+        public bool Save { get; set; }
+        public bool PrintName { get; set; }
+        public bool Enabled { get; set; }
+        public PinType Type { get; set; }
+
+    }
+}
---
+++ ValheimMod/ValheimMod/SpawnerPatchSpawn.cs
@@ -0,0 +1,37 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace ValheimMod
+{
+	[HarmonyPatch(typeof(SpawnArea), "Awake")]
+	class SpawnerPatchSpawn
+	{
+		private static void Postfix(ref SpawnArea __instance)
+		{
+			HoverText comp = __instance.GetComponent<HoverText>();
+			if (!comp)
+				return;
+			string compName = comp.m_text;
+			bool found = false;
+			Pin pin = new Pin();
+
+			foreach (Pin pinInfo in ValheimMod.pinsInfo)
+			{
+				if (pinInfo.Type != Pin.PinType.Spawner || !pinInfo.Enabled)
+					continue;
+				if (compName == pinInfo.ComponentName)
+				{
+					found = true;
+					pin = pinInfo;
+					break;
+				}
+			}
+
+			if (found && !ValheimMod.itemsPins.ContainsKey(comp.transform.position))
+			{
+				Minimap.instance.AddPin(comp.transform.position, Minimap.PinType.Icon3, ValheimMod.namePins.Value ? pin.Label : null, ValheimMod.savePins.Value && pin.Save, false, 0L);
+				ValheimMod.itemsPins.Add(comp.transform.position, pin.Label);
+			}
+		}
+	}
+}
